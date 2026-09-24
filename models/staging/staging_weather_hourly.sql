7cacc3a7d226dcc031a1a4658356caca19a9f177
shadishirinbeik <shadi.shirinbeik@gmail.com>
2026-09-24 15:38:47 +0200

    replace the column in weather hourly model

--- models/staging/staging_weather_hourly.sql
+++ models/staging/staging_weather_hourly.sql
@@ -6,21 +6,20 @@ WITH hourly_raw AS (
     FROM {{source('weather_data', 'weather_hourly_raw')}}
 ),
 hourly_flattened AS (
-    SELECT  
-        airport_code,
-        station_id,
-        (json_data ->> 'timestamp')::TIMESTAMP  AS timestamp,
-        (json_data ->> 'temp_c')::NUMERIC AS temp_c,
-        (json_data ->> 'dewpoint_c')::NUMERIC AS dewpoint_c,
-        (json_data ->> 'humidity_perc')::NUMERIC AS humidity_perc,
-        (json_data ->> 'precipitation_mm')::NUMERIC AS precipitation_mm,
-        (json_data ->> 'snow_mm')::INT AS snow_mm,
-        (json_data ->> 'wind_direction')::INT AS wind_direction,
-        (json_data ->> 'wind_speed_kmh')::  NUMERIC AS wind_speed_kmh,
-        (json_data ->> 'wind_peakgust_kmh')::NUMERIC AS wind_peakgust_kmh,
-        (json_data ->> 'pressure_hpa')::NUMERIC AS pressure_hpa,
-        (json_data ->> 'sun_minutes')::INT AS sun_minutes,
-        (json_data ->> 'condition_code')::INT AS condition_code
+    SELECT airport_code
+        ,station_id
+        ,(json_data ->> 'time')::TIMESTAMP AS timestamp
+        ,(json_data ->> 'temp')::NUMERIC AS temp_c
+        ,(json_data ->> 'dwpt')::NUMERIC AS dewpoint_c
+        ,(json_data ->> 'rhum')::NUMERIC AS humidity_perc
+        ,(json_data ->> 'prcp')::NUMERIC AS precipitation_mm
+        ,(json_data ->> 'snow')::NUMERIC::INTEGER AS snow_mm
+        ,(json_data ->> 'wdir')::NUMERIC::INTEGER AS wind_direction
+        ,(json_data ->> 'wspd')::NUMERIC AS wind_speed_kmh
+        ,(json_data ->> 'wpgt')::NUMERIC AS wind_peakgust_kmh
+        ,(json_data ->> 'pres')::NUMERIC AS pressure_hpa
+        ,(json_data ->> 'tsun')::NUMERIC::INTEGER AS sun_minutes
+        ,(json_data ->> 'coco')::NUMERIC::INTEGER AS condition_code
     FROM hourly_raw
 )
 SELECT *
