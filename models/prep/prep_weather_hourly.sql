ef7bcd4827e77faa059b1716b1419e31e6c9cd0c
shadishirinbeik <shadi.shirinbeik@gmail.com>
2026-09-24 16:04:38 +0200

    create weather hourly prep

---
+++ models/prep/prep_weather_hourly.sql
@@ -0,0 +1,25 @@
+WITH date_parts AS (
+    SELECT 
+    	timestamp::date AS date,
+    	timestamp::time AS time,
+    	DATE_PART('hour', timestamp) AS hour,
+    	TO_CHAR(timestamp, 'FMMonth') AS month_name,
+    	TO_CHAR(timestamp, 'FMDay') AS week_day,
+    	DATE_PART('day', timestamp) as date_day,
+    	DATE_PART('month', timestamp) as date_month,
+    	DATE_PART('year', timestamp) as date_year,
+    	DATE_PART('week', timestamp) as cw
+    FROM {{ref('staging_weather_hourly')}}
+),
+add_season AS (
+    SELECT *,
+        (CASE 
+            WHEN month_name IN ('November', 'December', 'January', 'February')
+            THEN 'winter'
+            WHEN month_name IN ('March', 'April', 'May') THEN 'spring'
+            WHEN month_name IN ('June', 'July', 'August') THEN 'sommer'
+            WHEN month_name IN ('September', 'October') THEN 'autumn'
+        END) AS season
+    FROM date_parts
+)
+select * from add_season
